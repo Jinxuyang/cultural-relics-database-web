691ad02d4fd3d82215579f7cc2cd9ef399aeb7d0
Verge <981340404@qq.com>
2021-11-08 15:06:50 +0800

    feat:

---
+++ src/plugins/element.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import {
+    Button,
+    Input,
+    Form,
+    FormItem,
+    Message,
+    Container,
+    Aside,
+    Main,
+    Footer,
+    Menu,
+    Submenu,
+    MenuItem,
+    MenuItemGroup,
+    Card,
+    Table,
+    TableColumn,
+    Tooltip,
+    Option,
+    Select,
+    Pagination,
+    Row,
+    Col,
+    Header,
+    PageHeader,
+    Upload
+} from 'element-ui'
+
+Vue.use(Input)
+Vue.use(Footer)
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.component(Message.name, Message)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(Menu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tooltip)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Pagination)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Header)
+Vue.use(PageHeader)
+Vue.use(Upload)
